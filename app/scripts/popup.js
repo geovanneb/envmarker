2d05131a514c6aff567b84806b488b16b99c5ea1
bertonha <gbertonha@ciandt.com>
2019-08-28 14:20:25 +0900

    Add a link to the configuration screen on the Environment Marker icon that is displayed besides the address bar.

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -7,7 +7,7 @@ function set_button_label() {
 	  		document.getElementById('popup-text').innerText = 'You have not set a label for this URL address.';
 	  		document.getElementById('popup-button').style.display = 'none';
 	  	} else {
-	  		document.getElementById('popup-text').innerText = 'Click on the following button to Hide/Show label:';
+	  		document.getElementById('popup-text').innerText = 'Click on the following button to temporarily Hide/Show the label:';
 	  		document.getElementById('popup-button').style.display = 'block';
 	  		document.getElementById('popup-button').innerText = response.isDisabled ? 'Show Label' : 'Hide Label';
 	  	}
@@ -25,5 +25,15 @@ function toggle_label() {
 	});
 }
 
+//Open configuration screen
+function open_configuration() {
+  if (chrome.runtime.openOptionsPage) {
+    chrome.runtime.openOptionsPage();
+  } else {
+    window.open(chrome.runtime.getURL('options.html'));
+  }
+}
+
 document.addEventListener('DOMContentLoaded', set_button_label);
 document.getElementById('popup-button').addEventListener('click', toggle_label);
+document.getElementById('go-to-options').addEventListener('click', open_configuration);
